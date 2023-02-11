9d8031089a82f33043037542d3faebec0a90234f
NNEUID <nneuid@gmail.com>
2023-02-11 23:34:36 +0000

    initialize firebase Auth before mouting app

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+import { auth } from './firebase/config'
+import { onAuthStateChanged } from 'firebase/auth'
+
+let app
+
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App).use(router).mount('#app')
+  }
+})
+
