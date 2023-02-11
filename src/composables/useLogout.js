d877be80460b002389a70ed7905bab34d77f6ae4
NNEUID <nneuid@gmail.com>
2023-02-11 20:22:53 +0000

    add useLogout compaosable

--- src/composables/useLogout.js
+++ src/composables/useLogout.js
@@ -1,10 +1,17 @@
 import { ref } from 'vue'
 import { auth } from '../firebase/config'
+import { signOut } from 'firebase/auth'
 
 const error = ref(null)
 
 const logout = async () => {
-
+  error.value = null
+  try {
+    await signOut(auth)
+  } catch (err) {
+    console.log(err);
+    error.value = err.message
+  }
 }
 
 const useLogout = () => {
